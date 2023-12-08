3a9ed14665cd10242a195f97a123b361bc048480
Ramazan Serhat UYGUN <92759220+Rsuygun@users.noreply.github.com>
2023-12-08 18:26:02 +0300

    @model directive

--- Basics/Controllers/EmployeeController.cs
+++ Basics/Controllers/EmployeeController.cs
@@ -1,22 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
+using Basics.Models;
 
 namespace Basics.Controllers
 {
     public class EmployeeController : Controller
     {
-        public String Index() 
+        public IActionResult Index1() 
         {
-            return "Hello World";
+            // "$" işareti string enterpolasyonu
+            string message = $"Hello World. {DateTime.Now.ToString()}";
+            return View("Index1", message);
         }
 
         public ViewResult Index2()
         {
-            return View("Index");
+            var names = new String[]
+            {
+                "Ahmet",
+                "Mehmet",
+                "Can"
+            };
+            return View(names);
         }
 
         public IActionResult Index3()
         {
-            return Content("Employee");
+            var list = new List<Employee>()
+            {
+                new Employee(){Id = 1, FirstName = "Ahmet", LastName = "Can", Age = 20},
+                new Employee(){Id = 2, FirstName = "Can", LastName = "Dağ", Age = 25},
+                new Employee(){Id = 3, FirstName = "Demir", LastName = "Güneş", Age = 37}
+            };
+            return View("Index3", list);
         }
     }
 }
