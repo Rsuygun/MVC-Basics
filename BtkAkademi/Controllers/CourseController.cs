59872c21da9f0e113508388357404f40c2a6bda9
Ramazan Serhat UYGUN <92759220+Rsuygun@users.noreply.github.com>
2023-12-09 23:06:18 +0300

    Feedback View

--- BtkAkademi/Controllers/CourseController.cs
+++ BtkAkademi/Controllers/CourseController.cs
@@ -21,7 +21,7 @@ namespace BtkAkademi.Controllers
         public IActionResult Apply([FromForm]Candidate model)
         {
             Repository.Add(model);
-            return Redirect("/");
+            return View("Feedback", model);
         }
     }
 }
