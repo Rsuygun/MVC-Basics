fdb26092b95f220a108b7e5563097387ad56e581
Ramazan Serhat UYGUN <92759220+Rsuygun@users.noreply.github.com>
2023-12-09 18:07:54 +0300

    Controller and View Definition

---
+++ BtkAkademi/Controllers/CourseController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BtkAkademi.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Apply()
+        {
+            return View();
+        }
+    }
+}
