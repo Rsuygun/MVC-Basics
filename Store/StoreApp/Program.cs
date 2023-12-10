1bd6f59034b774c582bf0d8cf699c4607c441fef
Ramazan Serhat UYGUN <92759220+Rsuygun@users.noreply.github.com>
2023-12-10 17:17:49 +0300

    Implementation of MVC Template Structure

--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -1,7 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/btk", () => "BTK Akademi");
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.MapControllerRoute(
+    "default",
+    "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
