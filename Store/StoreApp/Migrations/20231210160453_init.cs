de529b10b9d600f311f1f89758f5dec07d0ee646
Ramazan Serhat UYGUN <92759220+Rsuygun@users.noreply.github.com>
2023-12-10 19:34:26 +0300

    Migrations

---
+++ Store/StoreApp/Migrations/20231210160453_init.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PrudctName = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
--- Store/StoreApp/Models/Product.cs
+++ Store/StoreApp/Models/Product.cs
@@ -3,7 +3,7 @@ namespace StoreApp.Models
     public class Product
     {
         public int ProductId { get; set; }
-        public String? PrudctName { get; set; } = String.Empty;
+        public String? ProductName { get; set; } = String.Empty;
         public decimal Price { get; set; }
     }
 }
