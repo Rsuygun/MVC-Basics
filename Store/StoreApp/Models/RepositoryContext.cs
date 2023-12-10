b7198fff6d7990a12ade691c31d8c8c44cd649be
Ramazan Serhat UYGUN <92759220+Rsuygun@users.noreply.github.com>
2023-12-10 18:47:43 +0300

    ConnectionString

--- Store/StoreApp/Models/RepositoryContext.cs
+++ Store/StoreApp/Models/RepositoryContext.cs
@@ -5,5 +5,10 @@ namespace StoreApp.Models
     public class RepositoryContext : DbContext
     {
         public DbSet<Product> Products { get; set; }
+        public RepositoryContext(DbContextOptions<RepositoryContext> options)
+        : base(options)
+        {
+            
+        }
     }
 }
--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<RepositoryContext>(options => 
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"));
+});
 
 var app = builder.Build();
 
