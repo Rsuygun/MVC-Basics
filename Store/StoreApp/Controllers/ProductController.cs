1028787b6ade7d4cb6775e45f82b7e496f303179
Ramazan Serhat UYGUN <92759220+Rsuygun@users.noreply.github.com>
2023-12-11 20:45:21 +0300

    Inversion of Control

--- Store/StoreApp/Controllers/ProductController.cs
+++ Store/StoreApp/Controllers/ProductController.cs
@@ -18,7 +18,12 @@ namespace StoreApp.Controllers
 
             var model =  _context.Products.ToList();
             return View(model);
+        }
 
+        public IActionResult Get(int id)
+        {
+            Product product = _context.Products.First(p => p.ProductId.Equals(id));
+            return View(product);
         }
     }
 }
