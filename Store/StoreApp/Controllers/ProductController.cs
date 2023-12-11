3fabc8c364b598724b02c7789bd86eeebd89ff5d
Ramazan Serhat UYGUN <92759220+Rsuygun@users.noreply.github.com>
2023-12-11 20:14:00 +0300

    Dependency Injection

---
+++ Store/StoreApp/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
+
+namespace StoreApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly RepositoryContext? _context;
+
+        public ProductController(RepositoryContext? context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+
+            var model =  _context.Products.ToList();
+            return View(model);
+
+        }
+    }
+}
