38b8193c2f86d658c117b88b5888347917bed803
Ramazan Serhat UYGUN <92759220+Rsuygun@users.noreply.github.com>
2023-12-20 02:57:15 +0300

    (IProduct Service & ProductManager) & (ICategoryService & CategoryManager) & (IServiceManager & ServiceManager)

---
+++ Store/Services/ServiceManager.cs
@@ -0,0 +1,20 @@
+using Services.Contracts;
+
+namespace Services
+{
+    public class ServiceManager : IServiceManager
+    {
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+
+        public ServiceManager(IProductService productService, ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+            _productService = productService;
+        }
+
+        public IProductService ProductService => _productService;
+
+        public ICategoryService CategoryService => _categoryService;
+    }
+}
