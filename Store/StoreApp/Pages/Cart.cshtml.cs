48f5314896262e69869f6e802355e2c22e0c9a53
Ramazan Serhat UYGUN <92759220+Rsuygun@users.noreply.github.com>
2024-02-01 00:32:43 +0300

    _PageModel Implementation

--- Store/StoreApp/Pages/Cart.cshtml.cs
+++ Store/StoreApp/Pages/Cart.cshtml.cs
@@ -1,9 +1,40 @@
+using System.Runtime.CompilerServices;
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services.Contracts;
 
 namespace StoreApp.Pages
 {
     public class CartModel : PageModel
     {
-        
+        private readonly IServiceManager _manager;
+
+        public CartModel(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        public Cart Cart { get; set; } //IoC kaydi kullanilacak
+        public string ReturnUrl { get; set; } = "/";
+        public void OnGet(string returnUrl)
+        {
+            ReturnUrl = returnUrl ?? "/";
+        }
+        public IActionResult OnPost(int productId, string returnUrl)
+        {
+            Product? product = _manager.ProductService.GetOneProduct(productId, false);
+
+            if (product is not null)
+            {
+                Cart.AddItem(product, 1);
+            }
+            return Page();
+        }
+        public IActionResult OnPostRemove(int id, string returnUrl)
+        {
+            Cart.RemoveLine(Cart.Lines.First(cl => cl.Product.ProductId.Equals(id)).Product);
+            return Page();
+        }
     }
 }
