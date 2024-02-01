437ebc39aadf6001658bfe2f46b0528ad7e99457
Ramazan Serhat UYGUN <92759220+Rsuygun@users.noreply.github.com>
2024-02-02 01:56:26 +0300

    CartSummary Widget

---
+++ Store/StoreApp/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,18 @@
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreApp.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private readonly Cart _cart;
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            _cart = cartService;
+        }
+        public string Invoke()
+        {
+            return _cart.Lines.Count().ToString();
+        }
+    }
+}
