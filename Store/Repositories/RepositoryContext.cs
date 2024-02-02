559e02ba64789d0ab7ae34dc5c0eedf7f9872b5a
Ramazan Serhat UYGUN <92759220+Rsuygun@users.noreply.github.com>
2024-02-03 02:16:54 +0300

    Orders

--- Store/Repositories/RepositoryContext.cs
+++ Store/Repositories/RepositoryContext.cs
@@ -9,6 +9,7 @@ namespace Repositories
     {
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
         public RepositoryContext(DbContextOptions<RepositoryContext> options)
         : base(options)
         {
--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -30,10 +30,12 @@ builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 
 builder.Services.AddScoped<IServiceManager, ServiceManager>();
 builder.Services.AddScoped<IProductService, ProductManager>();
 builder.Services.AddScoped<ICategoryService, CategoryManager>();
+builder.Services.AddScoped<IOrderService, OrderManager>();
 
 builder.Services.AddScoped<Cart>(c => SessionCart.GetCart(c));
 
