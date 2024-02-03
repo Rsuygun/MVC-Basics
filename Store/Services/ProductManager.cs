354518bdf6b51835ba629f56e7bd292d25dd705b
Ramazan Serhat UYGUN <92759220+Rsuygun@users.noreply.github.com>
2024-02-04 01:29:25 +0300

    Showcase

--- Store/Services/ProductManager.cs
+++ Store/Services/ProductManager.cs
@@ -57,6 +57,12 @@ namespace Services
             return productDto;
         }
 
+        public IEnumerable<Product> GetShowcaseProducts(bool trackChanges)
+        {
+            var products = _manager.Product.GetShowcaseProducts(trackChanges);
+            return products;
+        }
+
         public void UpdateOneProduct(ProductDtoForUpdate productDto)
         {
             // var entity = _manager.Product.GetOneProduct(productDto.ProductId, true);
