9eb02df5d31f5813d65ce1ca2d979269a9e383b2
Ramazan Serhat UYGUN <92759220+Rsuygun@users.noreply.github.com>
2024-02-04 23:43:59 +0300

    TagBuilder

--- Store/Services/ProductManager.cs
+++ Store/Services/ProductManager.cs
@@ -47,6 +47,15 @@ namespace Services
             return _manager.Product.GetAllProductsWithDetails(p);
         }
 
+        public IEnumerable<Product> GetLastestProducts(int n, bool trackChanges)
+        {
+            return _manager
+                .Product
+                .FindAll(trackChanges)
+                .OrderByDescending(prd => prd.ProductId)
+                .Take(n);
+        }
+
         public Product? GetOneProduct(int id, bool trackChanges)
         {
             var product = _manager.Product.GetOneProduct(id, trackChanges);
---
+++ Store/StoreApp/Infrastructe/TagHelpers/LastestProductTagHelper.cs
@@ -0,0 +1,50 @@
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Services.Contracts;
+
+namespace StoreApp.Infrastructe.TagHelpers
+{
+    [HtmlTargetElement("div", Attributes ="products")]
+    public class LastestProductTagHelper : TagHelper
+    {
+        private readonly IServiceManager _manager;
+
+        public LastestProductTagHelper(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            TagBuilder div = new TagBuilder("div");
+            div.Attributes.Add("class", "my-3");
+
+            TagBuilder h6 = new TagBuilder("h6");
+            h6.Attributes.Add("class", "lead");
+
+            TagBuilder icon = new TagBuilder("i");
+            icon.Attributes.Add("class", "fa fa-box text-secondary");
+
+            h6.InnerHtml.AppendHtml(icon);
+            h6.InnerHtml.AppendHtml(" Lastest Products");
+
+            TagBuilder ul = new TagBuilder("ul");
+            var products = _manager.ProductService.GetLastestProducts(5, false);
+            foreach (Product product in products)
+            {
+                TagBuilder li = new TagBuilder("li");
+
+                TagBuilder a = new TagBuilder("a");
+                a.Attributes.Add("href", $"/product/get/{product.ProductId}");
+                a.InnerHtml.AppendHtml(product.ProductName);
+
+                li.InnerHtml.AppendHtml(a);
+                ul.InnerHtml.AppendHtml(li);
+            }
+
+            div.InnerHtml.AppendHtml(h6);
+            div.InnerHtml.AppendHtml(ul);
+            output.Content.AppendHtml(div);
+        }
+    }
+}
