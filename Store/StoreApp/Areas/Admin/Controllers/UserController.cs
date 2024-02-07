17280882303d4b9e81c9dced9369714a09bc2369
Ramazan Serhat UYGUN <92759220+Rsuygun@users.noreply.github.com>
2024-02-08 02:51:09 +0300

    User Update Page Design

--- Store/StoreApp/Areas/Admin/Controllers/UserController.cs
+++ Store/StoreApp/Areas/Admin/Controllers/UserController.cs
@@ -41,7 +41,19 @@ namespace StoreApp.Areas.Admin.Contollers
         }
         public async Task<IActionResult> Update([FromRoute(Name = "id")] string id)
         {
-            
+            var user = await _manager.AuthService.GetOneUserForUpdate(id);
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update([FromForm] UserDtoForUpdate userDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _manager.AuthService.Update(userDto);
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
